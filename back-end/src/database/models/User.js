e7fcea1f26a82d20dfc3d7c1fed7e1b17817f3f6
ArthurMGoulart <budiarthur@gmail.com>
2022-04-27 18:07:32 -0300

    feat: associate to sales

--- back-end/src/database/models/User.js
+++ back-end/src/database/models/User.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(models.Sales,
+        { foreignKey: 'user_id', as: 'sales' });
     }
   }
   User.init({
