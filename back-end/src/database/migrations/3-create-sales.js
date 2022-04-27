df5110a3c8e14b5677e5b8397cbe905c136bbdd7
ArthurMGoulart <budiarthur@gmail.com>
2022-04-27 18:07:51 -0300

    feat: migration Sales

---
+++ back-end/src/database/migrations/3-create-sales.js
@@ -0,0 +1,58 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      sellerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'seller_id',
+        references: {
+          model: 'salesProducts',
+          key: 'sale_id',
+        },
+      },
+      total_price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL(9,2)
+      },
+      delivery_address: {
+        allowNull: false,
+        type: Sequelize.STRING(100)
+      },
+      delivery_number: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      sale_date: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Sales');
+  }
+};
---
+++ back-end/src/database/models/Sale.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Sales extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.User, 
+        { foreignKey: 'user_id', as: 'user' } );
+    }
+  }
+  Sales.init({
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+    sellerId: { type: DataTypes.INTEGER, foreignKey: true },
+    total_price: DataTypes.DECIMAL,
+    delivery_address: DataTypes.STRING,
+    delivery_number: DataTypes.STRING,
+    sale_date: DataTypes.DATE,
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Sale',
+  });
+  return Sales;
+};
