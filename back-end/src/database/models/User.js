0acbec661a98f56ab39311791eb0c4d50ad70bc6
ArthurMGoulart <budiarthur@gmail.com>
2022-04-27 18:08:53 -0300

    fix: models.Sales -> models.Sale

--- back-end/src/database/models/User.js
+++ back-end/src/database/models/User.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.Sales,
+      this.hasMany(models.Sale,
         { foreignKey: 'user_id', as: 'sales' });
     }
   }
