1a6bf6c568c77310a477d695403136c1897541fc
ArthurMGoulart <budiarthur@gmail.com>
2022-04-27 15:56:00 -0300

    Iniciando projeto Delivery App

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/login');
+// app.get('/login', (_req, res) => res.status(200)));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
