7f0edff251903fc8730f6c642cc0b923066f5483
ArthurMGoulart <budiarthur@gmail.com>
2022-04-28 18:36:48 -0300

    feat: token

---
+++ back-end/src/token/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const jwtConfig = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
+
+const generateToken = (id, role) => {
+  const secret = 'teste';
+  const token = jwt.sign({ data: { id, role } }, secret, jwtConfig);
+  return { token };
+};
+
+module.exports = {
+  generateToken,
+};
