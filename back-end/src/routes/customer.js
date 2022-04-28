23c0e0ee351e801aa2c8338f3d822a4c5db88814
ArthurMGoulart <budiarthur@gmail.com>
2022-04-28 15:36:20 -0300

    docs: customer route docs

--- back-end/src/routes/customer.js
+++ back-end/src/routes/customer.js
@@ -5,6 +5,8 @@ const customerRouter = express.Router();
 // O fluxo do cliente deve garantir que seja possível:
 // - Navegar e escolher produtos
 // -- getAllProducts --getProductById
+// --- customerRouter.get('/products', auth, productController.getAll)
+// --- customerRouter.get('/products/:id', auth, productController.getById)
 // - Adicionar produtos ao carrinho
 // - Fazer checkout (gerar uma nova venda)
 // -- postSale
