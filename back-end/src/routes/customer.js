ca6cdc640d1b9aa70c3f9727724b431340672105
ArthurMGoulart <budiarthur@gmail.com>
2022-04-28 15:31:38 -0300

    feat, docs: customer route docs

---
+++ back-end/src/routes/customer.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const customerRouter = express.Router();
+
+// O fluxo do cliente deve garantir que seja possível:
+// - Navegar e escolher produtos
+// -- getAllProducts --getProductById
+// - Adicionar produtos ao carrinho
+// - Fazer checkout (gerar uma nova venda)
+// -- postSale
+// - Consultar pedidos e acessar detalhes do mesmo.
+// -- getSale
+
+module.exports = customerRouter;
