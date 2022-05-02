912475c049540338f77a83e69a6d1a05c354f105
ArthurMGoulart <budiarthur@gmail.com>
2022-05-02 18:40:32 -0300

    feat: integration tests

--- back-end/src/tests/controllers/login.test.js
+++ back-end/src/tests/controllers/login.test.js
@@ -3,12 +3,13 @@ const chai = require('chai');
 const chaiHTTP = require('chai-http');
 const userController = require('../../controllers/user')
 const userService = require('../../services/user');
+const app = require('../../api/app');
 
 chai.use(chaiHTTP);
 
 const { expect } = chai;
 
-describe('Test User Controller', () => {
+describe('Unit Test User Controller', () => {
 
   const mockRequest = {
     body: {
@@ -27,5 +28,43 @@ describe('Test User Controller', () => {
     const loginStub = sinon.stub(userService, 'login').resolves('Login');
     await userController.login(mockRequest, mockResponse);
     expect(loginStub.calledWith(mockRequest.body)).to.be.true;
+    loginStub.restore();
+  });
+}) 
+
+describe('Integration Test User Controller', () => {
+
+  const mockBodyCorrect = {
+    name: 'Delivery App Admin',
+    email: 'adm@deliveryapp.com',
+    password: '--adm2@21!!--',
+  };
+
+  const mockBodyIncorrect = {
+    name: 'Delivery App Admin',
+    email: 'adm@deliveryapp.com',
+    password: 'senhaerrada'
+  }
+
+  it('should return status 200 and token on correct body', () => {
+    chai.request(app)
+      .post('/login')
+      .send(mockBodyCorrect)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property(token);
+      });
+  });
+
+  it('should return status 404 and message not found on incorrect body', () => {
+    chai.request(app)
+      .post('/login')
+      .send(mockBodyIncorrect)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.not.have.property(token);
+      });
   });
 })
