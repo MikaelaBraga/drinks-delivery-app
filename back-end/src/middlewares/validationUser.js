e5bed02dfa70fddeda92574c2aa0acea20aba5f7
ArthurMGoulart <budiarthur@gmail.com>
2022-05-02 18:08:21 -0300

    feat: joi schemas

--- back-end/src/middlewares/validationUser.js
+++ back-end/src/middlewares/validationUser.js
@@ -1,20 +1,10 @@
+const { userRegister } = require('../schemas/userRegister');
+
 const validateBody = (req, res, next) => {
-  const { name, email, password } = req.body;
-  const regex = /^(.+)@(.+)$/;
-  const verifyEmail = email.match(regex);
+  const { error } = userRegister.validate(req.body);
 
-  if (name.length < 12) {
-    return res.status(400).json({ message: '"name" length must be at least 8 characters long' });
-  }
-
-  if (!verifyEmail) {
-    return res.status(400).json({ message: '"email" must be a valid email' });
-  }
-
-  if (password.length < 6) {
-    return res.status(400).json(
-      { message: '"password" length must be at least 6 characters long' }
-    );
+  if (error) {
+    return res.status(400).json({ error: error.message });
   }
 
   next();
---
+++ back-end/src/schemas/userRegister.js
@@ -0,0 +1,21 @@
+const joi = require('joi');
+
+const userRegister = joi.object({
+  name: joi.string().min(12).required().messages({
+    'string.base': 'name must be a string',
+    'string.min': 'name length must be at least 12 characters long',
+    'any.required': 'name is required',
+  }),
+  email: joi.string().required().email().messages({
+    'string.base': 'email must be a string',
+    'string.email': 'email must be a valid email',
+    'any.required': 'email is required',
+  }),
+  password: joi.string().min(6).required().messages({
+    'string.base': 'password must be a string',
+    'string.min': 'password must be longer than 6 characters',
+    'any.required': 'password is required',
+  }),
+});
+
+module.exports = { userRegister };
