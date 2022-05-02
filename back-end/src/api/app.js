101847699af1357eb00a1c91616152b2bc9582f6
Matheus <matheushtc25@gmail.com>
2022-05-02 17:05:55 -0300

    feat: adding the route POST /register.

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -5,10 +5,12 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const loginRouter = require('../routes/login');
+const registerRouter = require('../routes/register');
 
 app.use(bodyParser.json());
 
 app.use('/login', loginRouter);
+app.use('/register', registerRouter);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
--- back-end/src/routes/register.js
+++ back-end/src/routes/register.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const registerRouter = express.Router();
 
+const userController = require('../controllers/user');
+const { validateBody } = require('../middlewares/validationUser');
+
 // Adicionar rotas:
 // -post
+
+registerRouter.post(
+  '/',
+  validateBody,
+  userController.register
+);
 
 module.exports = registerRouter;
