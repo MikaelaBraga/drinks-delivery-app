dc56d546fe06e5e8bb6920fa23da54d496439083
Matheus <matheushtc25@gmail.com>
2022-05-02 19:34:35 -0300

    refactor: adding default value to role.

--- back-end/src/services/user.js
+++ back-end/src/services/user.js
@@ -18,12 +18,16 @@ const login = async (user) => {
 };
 
 const register = async (user) => {
-  const { email, password, role } = user;
-
+  const { email, password, role: roleUser } = user;
   const userFound = await getByEmail(email);
   if (userFound) return null;
+  
+  let role = roleUser;
+  if (!role) {
+    role = 'customer';
+  }
 
-  const { id } = await User.create({ ...user, password: md5(password) });
+  const { id } = await User.create({ ...user, password: md5(password), role });
   
   const token = generateToken(id, role);
 
