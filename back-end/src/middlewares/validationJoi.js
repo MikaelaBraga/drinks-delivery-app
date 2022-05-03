a98174433c780d4311193d81f496bd0ab1069785
ArthurMGoulart <budiarthur@gmail.com>
2022-05-03 15:47:47 -0300

    fix: lint

--- back-end/src/middlewares/validationJoi.js
+++ back-end/src/middlewares/validationJoi.js
@@ -1,11 +1,9 @@
 // ref: https://dev.to/tayfunakgc/middleware-based-joi-validation-in-expressjs-2po5
-const validationJoi = (schema) => {
-  return function(req, res, next) {
+const validationJoi = (schema) => (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) return res.status(400).json({ message: error.message });
     next();
-  }
-}
+};
 
 module.exports = {
   validationJoi,
