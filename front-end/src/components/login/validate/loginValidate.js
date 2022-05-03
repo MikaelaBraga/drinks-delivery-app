95e8f8423ca5a3b43e18bdf73af3f316938e5cfa
MIkaela Braga <bragamikaela@gmail.com>
2022-05-03 17:54:39 -0300

    feat: adding more input login validations

--- front-end/src/components/login/validate/loginValidate.js
+++ front-end/src/components/login/validate/loginValidate.js
@@ -2,14 +2,16 @@ import * as yup from 'yup';
 
 const FORTY = 40;
 const THIRTYTWO = 32;
+const SIX = 6;
 
 const loginValidate = yup.object().shape({
-  email: yup.string()
+  email: yup.string().email('Campo inválido!')
     .required(' O campo email é obrigatório!')
-    .max(FORTY, 'Campo inválido!'),
+    .max(FORTY, 'Campo inválido.'),
   password: yup.string()
     .required('O campo senha é obrigatório!')
-    .max(THIRTYTWO, 'Campo inválido!'),
+    .min(SIX, 'Campo inválido!')
+    .max(THIRTYTWO, 'Campo inválido.'),
 });
 
 export default loginValidate;
