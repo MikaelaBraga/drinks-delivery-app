dc1f7f84a80bc19ba9eadef2b0e8eaea4e6b6bfc
sthecso <sthecarol0411@gmail.com>
2022-05-03 20:18:02 -0300

    fix: register validate

--- front-end/src/components/register/validate/registerValidate.js
+++ front-end/src/components/register/validate/registerValidate.js
@@ -3,17 +3,17 @@ import * as yup from 'yup';
 const TWELVE = 12;
 const SIX = 6;
 
-const loginValidate = yup.object().shape({
+const registerValidate = yup.object().shape({
   name: yup.string()
     .required(' O campo Nome é de preenchimento obrigatório!')
-    .min(TWELVE, 'O campo Nome não é válido'),
+    .min(TWELVE, 'O campo Nome deve ter mais de 12 caracteres'),
   email: yup.string()
-    .required(' O campo é de preenchimento obrigatório!')
+    .required('O campo Email é de preenchimento obrigatório!')
     .email(/\S+@\S+\.\S+/,
       'O endereço usado no campo Email não é um endereço de email válido'),
   password: yup.string()
-    .required('O campo senha é obrigatório!')
+    .required('O campo Senha é de preenchimento obrigatório!')
     .min(SIX, 'Sua senha deve ter no mínimo 6 caracteres'),
 });
 
-export default loginValidate;
+export default registerValidate;
