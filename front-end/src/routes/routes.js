d4d0d684e7abc998fead7e99fca898c861e52c6b
sthecso <sthecarol0411@gmail.com>
2022-05-03 18:45:10 -0300

    feat: create register route

--- front-end/src/routes/routes.js
+++ front-end/src/routes/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 function Routers() {
   return (
@@ -8,6 +9,7 @@ function Routers() {
       <Routes>
         <Route exact path="/" element={ <Navigate to="/login" /> } />
         <Route path="/login" element={ <Login /> } />
+        <Route path="/register" element={ <Register /> } />
       </Routes>
     </BrowserRouter>
   );
