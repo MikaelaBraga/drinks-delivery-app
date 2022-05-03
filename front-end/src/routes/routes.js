fdfdfe082ba7a67f16d31169dfe395ffa99812db
MIkaela Braga <bragamikaela@gmail.com>
2022-05-02 23:41:55 -0300

    refactor: remove BrowserRouter Alias from import

--- front-end/src/routes/routes.js
+++ front-end/src/routes/routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Login';
 
 function Routers() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route exact path="/" element={ <Navigate to="/login" /> } />
         <Route path="/login" element={ <Login /> } />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
