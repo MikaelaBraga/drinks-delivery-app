01cd5969bd0571b571879c006e628eaafc71b355
ArthurMGoulart <budiarthur@gmail.com>
2022-05-03 14:22:14 -0300

    feat: cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -8,6 +8,15 @@ const loginRouter = require('../routes/login');
 
 app.use(bodyParser.json());
 
+// cors
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 app.use('/login', loginRouter);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
