2a8a76abbd5e94f46e64a82b8e788bcde11b704b
MIkaela Braga <bragamikaela@gmail.com>
2022-05-03 14:15:53 -0300

    fix: fix eslint magic number

--- front-end/src/components/login/validate/loginValidate.js
+++ front-end/src/components/login/validate/loginValidate.js
@@ -1,12 +1,15 @@
 import * as yup from 'yup';
 
+const FORTY = 40;
+const THIRTYTWO = 32;
+
 const loginValidate = yup.object().shape({
   email: yup.string()
-    // eslint-disable-next-line no-magic-numbers
-    .required(' O campo email é obrigatório!').max(40, 'Campo inválido!'),
+    .required(' O campo email é obrigatório!')
+    .max(FORTY, 'Campo inválido!'),
   password: yup.string()
-    // eslint-disable-next-line no-magic-numbers
-    .required('O campo senha é obrigatório!').max(32, 'Campo inválido!'),
+    .required('O campo senha é obrigatório!')
+    .max(THIRTYTWO, 'Campo inválido!'),
 });
 
 export default loginValidate;
