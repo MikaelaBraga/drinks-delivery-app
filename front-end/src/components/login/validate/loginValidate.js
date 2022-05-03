d7cc0f2ad3485593b6a5e282fe98db5ffd295d2c
MIkaela Braga <bragamikaela@gmail.com>
2022-05-03 00:57:23 -0300

    feat: customize required fields messages

--- front-end/src/components/login/validate/loginValidate.js
+++ front-end/src/components/login/validate/loginValidate.js
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
 const loginValidate = yup.object().shape({
-  email: yup.string().required(),
-  password: yup.string().required(),
+  email: yup.string().required(' O campo email é obrigatório!'),
+  password: yup.string().required('O campo senha é obrigatório!'),
 });
 
 export default loginValidate;
