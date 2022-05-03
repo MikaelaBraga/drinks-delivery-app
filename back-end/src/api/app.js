46a204bf48e6a079d970687141a07f30174aa01f
ArthurMGoulart <budiarthur@gmail.com>
2022-05-03 14:28:38 -0300

    feat: bodyParser to express.json

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,18 +2,17 @@ const express = require('express');
 
 const app = express();
 
-const bodyParser = require('body-parser');
 const loginRouter = require('../routes/login');
 
-app.use(bodyParser.json());
 // cors
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
+
+app.use(express.json());
 
 app.use('/login', loginRouter);
 
