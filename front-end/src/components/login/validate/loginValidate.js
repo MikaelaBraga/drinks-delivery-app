a8d6cf171350485f011dcd35dd697fc27d0c8380
MIkaela Braga <bragamikaela@gmail.com>
2022-05-03 01:02:03 -0300

    feat: customize length fields and messages

--- front-end/src/components/login/validate/loginValidate.js
+++ front-end/src/components/login/validate/loginValidate.js
@@ -1,8 +1,12 @@
 import * as yup from 'yup';
 
 const loginValidate = yup.object().shape({
-  email: yup.string().required(' O campo email é obrigatório!'),
-  password: yup.string().required('O campo senha é obrigatório!'),
+  email: yup.string()
+    // eslint-disable-next-line no-magic-numbers
+    .required(' O campo email é obrigatório!').max(40, 'Campo inválido!'),
+  password: yup.string()
+    // eslint-disable-next-line no-magic-numbers
+    .required('O campo senha é obrigatório!').max(32, 'Campo inválido!'),
 });
 
 export default loginValidate;
