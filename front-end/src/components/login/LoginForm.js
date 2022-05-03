89bfe395a4ce6c57230d90bb3f5a84d120215763
MIkaela Braga <bragamikaela@gmail.com>
2022-05-03 17:26:01 -0300

    feat: implements email and password validate with backend response

--- front-end/src/components/login/LoginForm.js
+++ front-end/src/components/login/LoginForm.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import loginValidate from './validate/loginValidate';
 import api from '../../services/api';
 
 function Login() {
+  const [invalidLogin, setInvalidLogin] = useState();
   const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: yupResolver(loginValidate),
   });
 
   const onSubmit = (data) => api.post('/login', data)
     .then(() => console.log('Deu certo!'))
-    .catch((err) => console.log(err));
+    .catch((err) => setInvalidLogin(err.response));
 
   return (
     <form onSubmit={ handleSubmit(onSubmit) }>
@@ -34,6 +35,9 @@ function Login() {
           { ...register('password') }
         />
         <p>{ errors.password?.message }</p>
+      </div>
+      <div>
+        <p>{ invalidLogin?.statusText }</p>
       </div>
       <button
         data-testid="common_login__button-login"
