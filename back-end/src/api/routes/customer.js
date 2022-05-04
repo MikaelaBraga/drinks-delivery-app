b53aceec56cb31c6b69a9748fe32d830426cc9fc
ArthurMGoulart <budiarthur@gmail.com>
2022-05-04 17:05:01 -0300

    feat: /products/:id route

--- back-end/src/api/routes/customer.js
+++ back-end/src/api/routes/customer.js
@@ -12,6 +12,7 @@ const productController = require('../controllers/product');
 customerRouter.get('/products', validationCustomer, productController.getAll);
 // --- customerRouter.get('/products', auth, productController.getAll)
 // --- customerRouter.get('/products/:id', auth, productController.getById)
+customerRouter.get('/products/:id', validationCustomer, productController.getById);
 // - Adicionar produtos ao carrinho
 // - Fazer checkout (gerar uma nova venda)
 // -- postSale
