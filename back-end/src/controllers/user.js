e9b79106e2dfb906b69b14e28628d483e5c81a80
Matheus <matheushtc25@gmail.com>
2022-05-04 11:36:09 -0300

    refactor: changing the status code of when there is a registered email.

--- back-end/src/controllers/user.js
+++ back-end/src/controllers/user.js
@@ -15,7 +15,7 @@ const registerCustomer = async (req, res) => {
   const { body } = req;
   try {
     const token = await userService.registerCustomer(body);
-    if (!token) return res.status(422).json({ message: 'E-mail already registered' });
+    if (!token) return res.status(409).json({ message: 'E-mail already registered' });
     return res.status(201).json(token);
   } catch (e) {
     return res.status(500).send(e.message);
