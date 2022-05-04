384f22fbd591cc51024cd4ac320fda48d45645db
ArthurMGoulart <budiarthur@gmail.com>
2022-05-04 15:23:18 -0300

    fix: lint

--- back-end/src/api/services/product.js
+++ back-end/src/api/services/product.js
@@ -1,4 +1,4 @@
-const { Product } = require('../database/models');
+const { Product } = require('../../database/models');
 
 const createNew = async (product) => {
   const newProduct = await Product.create(product);
