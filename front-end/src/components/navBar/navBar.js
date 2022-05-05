142a3ea1c286460d66375caa42775715018a97c0
sthecso <sthecarol0411@gmail.com>
2022-05-05 17:10:07 -0300

    fix: change li name and add classes container and ul-container

--- front-end/src/components/navBar/navBar.js
+++ front-end/src/components/navBar/navBar.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>
-      <ul>
+    <div class="container">
+      <ul class="ul-container">
         <li>
           <Link
             data-testid="customer_products__element-navbar-link-products"
@@ -21,10 +21,8 @@ function Navbar() {
             Meus pedidos
           </Link>
         </li>
-        <li>
-          <p data-testid="customer_products__element-navbar-user-full-name">
-            nome
-          </p>
+        <li data-testid="customer_products__element-navbar-user-full-name">
+          nome
         </li>
         <li>
           <Link to="/"> Sair </Link>
