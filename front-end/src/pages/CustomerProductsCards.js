0acd1dfa62dc8eee85e911f8f3769f5d2211a3c1
MIkaela Braga <bragamikaela@gmail.com>
2022-05-05 22:10:14 -0300

    feat: implements base context configuration and add customerProductsCards page and route

---
+++ front-end/src/pages/CustomerProductsCards.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProductCard from '../components/customerProducts/CustomerProducts';
+
+function CustomerProductsCards() {
+  return (
+    <ProductCard />
+  );
+}
+
+export default CustomerProductsCards;
