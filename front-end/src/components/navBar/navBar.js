e9fff49af05ae240452435fd3a6fa860b8edb5d1
sthecso <sthecarol0411@gmail.com>
2022-05-09 18:38:33 -0300

    feat: add navbar to seller and admin

--- front-end/src/components/navBar/navBar.js
+++ front-end/src/components/navBar/navBar.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './navBar.module.css';
 
 function Navbar() {
-  return (
-    <nav className={ styles.navBar }>
-      <ul className={ styles.list }>
+  const {
+    name,
+    role,
+    isUser,
+    isAdmin,
+    isSeller,
+    setIsUser,
+    setIsSeller,
+    setIsAdmin,
+  } = useContext(Context);
+
+  useEffect(() => {
+    switch (role) {
+    case 'user':
+      setIsUser(true);
+      break;
+    case 'seller':
+      setIsSeller(true);
+      break;
+    case 'admin':
+      setIsAdmin(true);
+      break;
+    default:
+      break;
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function navBarUser() {
+    return (
+      <>
         <li className={ styles.item }>
           <Link
             data-testid="customer_products__element-navbar-link-products"
@@ -22,13 +50,52 @@ function Navbar() {
             Meus pedidos
           </Link>
         </li>
+      </>
+    );
+  }
+
+  function navBarSeller() {
+    return (
+      <li className={ styles.item }>
+        <Link
+          data-testid="customer_products__element-navbar-link-orders"
+          to="/seller/orders"
+        >
+          Pedidos
+        </Link>
+      </li>
+    );
+  }
+
+  function navBarAdmin() {
+    return (
+      <li className={ styles.item }>
+        <Link
+          data-testid="customer_products__element-navbar-link-products"
+          to="/admin/manage"
+        >
+          Gerenciar Usuários
+        </Link>
+      </li>
+    );
+  }
+
+  return (
+    <nav className={ styles.navBar }>
+      <ul className={ styles.list }>
+        { isUser && navBarUser() }
+        { isSeller && navBarSeller() }
+        { isAdmin && navBarAdmin() }
         <li
           className={ styles.item }
           data-testid="customer_products__element-navbar-user-full-name"
         >
-          nome
+          { name }
         </li>
-        <li className={ styles.item }>
+        <li
+          className={ styles.item }
+          data-testid="customer_products__element-navbar-link-logout"
+        >
           <Link to="/"> Sair </Link>
         </li>
       </ul>
