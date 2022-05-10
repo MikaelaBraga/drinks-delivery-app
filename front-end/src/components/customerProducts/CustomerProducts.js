8740f20a96d44cb7de6ca3c8079234752c2327ee
MIkaela Braga <bragamikaela@gmail.com>
2022-05-09 23:05:58 -0300

    style: add cards products simple styles

--- front-end/src/components/customerProducts/CustomerProducts.js
+++ front-end/src/components/customerProducts/CustomerProducts.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import api from '../../services/api';
 import { CartContext } from '../../context/CartProvider';
 import CartTotalPrice from '../../hooks/CartTotalPrice';
+import './CustomerProducts.css';
 
 function ProductCard() {
   const [products, setProducts] = useState([]);
@@ -20,57 +21,59 @@ function ProductCard() {
 
   return (
     <div>
-      { products.map((product, index) => (
-        <div key={ index }>
-          <h3
-            data-testid={ `customer_products__element-card-title-${product.id}` }
-          >
-            { product.name }
-          </h3>
-          <img
-            data-testid={ `customer_products__img-card-bg-image-${product.id}` }
-            src={ product.url_image }
-            alt="imagem do produto"
-          />
-          <h5
-            data-testid={ `customer_products__element-card-price-${product.id}` }
-          >
-            { `${product.price}`.replace('.', ',') }
-          </h5>
-          <button
-            data-testid={ `customer_products__button-card-add-item-${product.id}` }
-            type="button"
-            onClick={ () => addCheckoutItem(product.id, product.name, product.price) }
-          >
-            +
-          </button>
-          <input
-            type="number"
-            data-testid={ `customer_products__input-card-quantity-${product.id}` }
-            value={ cart.find((c) => c.productId === product.id)
-              ? cart.find((c) => c.productId === product.id)?.quantity : 0 }
-          />
-          <button
-            data-testid={ `customer_products__button-card-rm-item-${product.id}` }
-            type="button"
-            onClick={ () => removeCheckoutItem(product.id) }
-          >
-            -
-          </button>
-        </div>
-      )) }
-
-      <div>
-        <Link to="/customer/checkout">
-          <button
-            data-testid="customer_products__checkout-bottom-value"
-            type="button"
-            disabled={ totalPrice === 0 }
-          >
-            {`${parseFloat(totalPrice).toFixed(2)}`.replace('.', ',')}
-          </button>
-        </Link>
+      <div className="products">
+        { products.map((product, index) => (
+          <div className="card" key={ index }>
+            <img
+              data-testid={ `customer_products__img-card-bg-image-${product.id}` }
+              src={ product.url_image }
+              alt="imagem do produto"
+            />
+            <h3
+              data-testid={ `customer_products__element-card-title-${product.id}` }
+            >
+              { product.name }
+            </h3>
+            <h5
+              data-testid={ `customer_products__element-card-price-${product.id}` }
+            >
+              { `${product.price}`.replace('.', ',') }
+            </h5>
+            <div className="counter-product">
+              <button
+                data-testid={ `customer_products__button-card-add-item-${product.id}` }
+                type="button"
+                onClick={ () => addCheckoutItem(product.id, product.name, product.price) }
+              >
+                +
+              </button>
+              <input
+                type="number"
+                data-testid={ `customer_products__input-card-quantity-${product.id}` }
+                value={ cart.find((c) => c.productId === product.id)
+                  ? cart.find((c) => c.productId === product.id)?.quantity : 0 }
+              />
+              <button
+                data-testid={ `customer_products__button-card-rm-item-${product.id}` }
+                type="button"
+                onClick={ () => removeCheckoutItem(product.id) }
+              >
+                -
+              </button>
+            </div>
+          </div>
+        )) }
       </div>
+      <button
+        className="cart-button"
+        data-testid="customer_products__checkout-bottom-value"
+        type="button"
+        disabled={ totalPrice === 0 }
+      >
+        <Link to="/customer/checkout">
+          {`${parseFloat(totalPrice).toFixed(2)}`.replace('.', ',')}
+        </Link>
+      </button>
     </div>
   );
 }
