d6b1646889d90f7740c7563d2465b799530c15c3
sthecso <sthecarol0411@gmail.com>
2022-05-11 16:38:23 -0300

    feat: make folder hooks in components

---
+++ front-end/src/components/hooks/products/ItemQuantityProducts.js
@@ -0,0 +1,25 @@
+// implementar algumas refatrações nesse arquivo
+
+// import { useContext, useEffect, useState } from 'react';
+// import { CartContext } from '../context/CartProvider';
+
+// function ItemQuantityCheckout() {
+//   const [itemQuantity, setItemQuantity] = useState(0);
+//   const { cart } = useContext(CartContext);
+
+//   useEffect(() => {
+//     console.log(itemQuantity);
+
+// const verifyProductCart = cart.find((c) => c.productId === id);
+
+// if (verifyProductCart) {
+//   const getQuantity = cart.find((c) => c.productId === id)?.quantity;
+//   setItemQuantity(getQuantity);
+// } else {
+// }
+//   }, [cart, itemQuantity]);
+
+//   return [itemQuantity, setItemQuantity];
+// }
+
+// export default ItemQuantityCheckout;
