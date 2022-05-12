f5bddde0561dcceec12b439e662527bd0e66091b
MIkaela Braga <bragamikaela@gmail.com>
2022-05-11 23:47:26 -0300

    feat: add input validations with yup and react hook form

--- front-end/src/components/customerCheckout/DeliveryDetails.js
+++ front-end/src/components/customerCheckout/DeliveryDetails.js
@@ -1,36 +1,63 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useNavigate, Link } from 'react-router-dom';
 import api from '../../services/api';
 import GetCheckout from '../hooks/checkout/GetCheckout';
+import GetSellers from '../hooks/checkout/GetSellers';
 
 function DeliveryDetails() {
   const { token } = JSON.parse(localStorage.getItem('user'));
   const [cartCheckout] = GetCheckout();
-  const { register, handleSubmit } = useForm();
+  const [sellers] = GetSellers();
+
+  const { register, handleSubmit, formState: { errors, isDirty, isValid } } = useForm({
+    resolver: yupResolver(loginValidate),
+    mode: 'onChange',
+  });
 
   function sendOrders(datas) {
     const { adress, number } = datas;
-    const order = { products: [],  }
+    const order = { products: [] };
 
     api.post('/customer/order', datas,
-    { headers: { Authorization: token } }).then().catch();
+      { headers: { Authorization: token } }).then().catch();
   }
 
   const onSubmit = (datas) => sendOrders(datas);
 
   return (
     <form onSubmit={ handleSubmit(onSubmit) }>
-      <label>P. Vendedora Responsável:
-        <select>
-          <option>{}</option>
+      <label htmlFor="seller">
+        P. Vendedora Responsável:
+        <select name="seller">
+          { sellers.map((seller, index) => (
+            <option key={ index }>{ seller.name }</option>
+          )) }
         </select>
       </label>
-      <label>Endereço
-        <input type="text" />
+      <label htmlFor="adress">
+        Endereço
+        <input
+          type="text"
+          name="adress"
+          placeholder="Rua Xablau, Bairro Xablauzinho"
+          { ...register('adress') }
+        />
       </label>
-      <label>Número
-        <input type="number" />
+      <label htmlFor="numberAdress">
+        Número
+        <input
+          type="number"
+          name="numberAdress"
+          placeholder="198"
+          { ...register('numberAdress') }
+        />
       </label>
+
+      <strong>{ errors.adress?.message || errors.numberAdress?.message }</strong>
+
+      <button type="submit" disabled={ !isDirty || !isValid }>FINALIZAR PEDIDO</button>
     </form>
   );
 }
