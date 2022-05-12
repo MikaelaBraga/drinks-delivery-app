1c58d1bc16848a12c93e1af2b5f99e7fc47b5d5a
ArthurMGoulart <budiarthur@gmail.com>
2022-05-12 19:09:09 -0300

    feat: route /seller/orders

---
+++ front-end/src/pages/SellerOrders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import SaleCardSeller from '../components/sale/saleCardSeller';
+import Navbar from '../components/navBar/navBar';
+
+function SellerOrders() {
+  return (
+    <Navbar />
+  );
+}
+
+export default SellerOrders;
