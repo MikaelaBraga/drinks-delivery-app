eb8f29ac97c17bedd8307df876c3ab0cd55ccfdf
ArthurMGoulart <budiarthur@gmail.com>
2022-05-12 19:05:47 -0300

    fix: lint

--- front-end/src/components/sale/saleCardSeller.js
+++ front-end/src/components/sale/saleCardSeller.js
@@ -1,8 +1,11 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
 function SaleCardSeller(props) {
   const { sale, index } = props;
-  const { id, status, date, totalPrice, deliveryAddress, deliveryNumber } = sale;
+  const { id, status, saleDate, totalPrice, deliveryAddress, deliveryNumber } = sale;
   return (
-    <div className="saleCard" key={index}>
+    <div className="saleCard" key={ index }>
       <div data-testid={ `seller_orders__element-order-id-${id}` }>
         <h1>Pedido</h1>
         <h2>{id}</h2>
@@ -10,17 +13,29 @@ function SaleCardSeller(props) {
       <div data-testid={ `seller_orders__element-delivery-status-${id}` }>
         <h1>{status}</h1>
       </div>
-      <div data-testid={ `seller_orders__element-order-date-${id}`}>
-        <h1>{date}</h1>
+      <div data-testid={ `seller_orders__element-order-date-${id}` }>
+        <h1>{saleDate}</h1>
       </div>
-      <div data-testid={ `seller_orders__element-card-price-${id}`}>
+      <div data-testid={ `seller_orders__element-card-price-${id}` }>
         <h1>{totalPrice}</h1>
       </div>
-      <div data-testid={ `seller_orders__element-card-address-${id}`}>
+      <div data-testid={ `seller_orders__element-card-address-${id}` }>
         <h3>{ `${deliveryAddress}, ${deliveryNumber}`}</h3>
       </div>
     </div>
   );
 }
+
+SaleCardSeller.propTypes = {
+  sale: PropTypes.shape({
+    id: PropTypes.number,
+    status: PropTypes.string,
+    saleDate: PropTypes.string,
+    totalPrice: PropTypes.number,
+    deliveryAddress: PropTypes.string,
+    deliveryNumber: PropTypes.number,
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+};
 
 export default SaleCardSeller;
