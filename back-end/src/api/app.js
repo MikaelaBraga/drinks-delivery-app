42b949f3b38b61bd1965559c76a9b2499dd40aee
Matheus <matheushtc25@gmail.com>
2022-05-13 16:02:56 -0300

    feat: implementing GET /user route.

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -8,6 +8,7 @@ const loginRouter = require('./routes/login');
 const registerRouter = require('./routes/register');
 const customerRouter = require('./routes/customer');
 const sellerRouter = require('./routes/seller');
+const userRouter = require('./routes/user');
 
 // Acess Control
 app.use((_req, res, next) => {
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/customer', customerRouter);
+app.use('/user', userRouter);
 app.use('/seller', sellerRouter);
 app.use('/images', express.static(path.join(__dirname, '../../public/uploads/')));
 app.get('/coffee', (_req, res) => res.status(418).end());
