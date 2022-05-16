78bd61d7cf7fa659afe3b49eb90fc07f36ae04d1
sthecso <sthecarol0411@gmail.com>
2022-05-16 15:59:45 -0300

    feat: page order details

---
+++ front-end/src/pages/OrderDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/navBar/navBar';
+import TableOrderDetails from '../components/order-details/tableOrderDetails';
+
+function OrderDetails() {
+  return (
+    <>
+      <Navbar />
+      <TableOrderDetails />
+    </>
+  );
+}
+
+export default OrderDetails;
