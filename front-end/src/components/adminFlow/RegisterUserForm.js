539db79e15f56331bf0e845d08ff60b17d8242e3
MIkaela Braga <bragamikaela@gmail.com>
2022-05-17 23:29:03 -0300

    feat: initializes react-hook-form implementation

--- front-end/src/components/adminFlow/RegisterUserForm.js
+++ front-end/src/components/adminFlow/RegisterUserForm.js
@@ -1,34 +1,58 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 function RegisterUserFormAdmin() {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(deliveryDetailsValidate),
+    mode: 'onChange',
+  });
+
+  const onSubmit = (datas) => console.log(datas);
+
   return (
     <>
       <h1>Cadastrar novo usuário</h1>
 
-      <form>
+      <form onSubmit={ handleSubmit(onSubmit) }>
         <label htmlFor="name">
           Nome
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="name"
+            { ...register('name') }
+          />
         </label>
         <label htmlFor="email">
           Email
-          <input type="text" name="email" />
+          <input
+            type="text"
+            name="email"
+            { ...register('email') }
+          />
         </label>
         <label htmlFor="password">
           Senha
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            { ...register('password') }
+          />
         </label>
-        <label htmlFor="type-user">
+        <label htmlFor="role">
           Tipo
-          <select name="type-user">
+          <select name="role" { ...register('role') }>
             <option value="Vendedor">Vendedor</option>
             <option valeu="Cliente">Cliente</option>
           </select>
         </label>
         <button type="submit">CADASTRAR</button>
+        <strong>
+          { errors.name?.message
+          || errors.email?.message
+          || errors.password?.message
+          || errors.role?.message }
+        </strong>
       </form>
     </>
   );
