0db1e37a3162d8a09ad5c0fcb3fbc08e7e4968bb
MIkaela Braga <bragamikaela@gmail.com>
2022-05-19 21:18:12 -0300

    style: add simple styles in order card

--- front-end/src/components/sale/saleCardSeller.js
+++ front-end/src/components/sale/saleCardSeller.js
@@ -1,26 +1,40 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import './saleCard.css';
 
 function SaleCardSeller(props) {
   const { sale, index } = props;
   const { id, status, saleDate, totalPrice, deliveryAddress, deliveryNumber } = sale;
   return (
     <div className="saleCard" key={ index }>
-      <div data-testid={ `seller_orders__element-order-id-${id}` }>
+      <div
+        className="orderId"
+        data-testid={ `seller_orders__element-order-id-${id}` }
+      >
         <p>Pedido</p>
         <span>{id}</span>
       </div>
-      <div data-testid={ `seller_orders__element-delivery-status-${id}` }>
+      <div
+        className="statusCard"
+        data-testid={ `seller_orders__element-delivery-status-${id}` }
+      >
         <h2>{status}</h2>
       </div>
-      <div data-testid={ `seller_orders__element-order-date-${id}` }>
-        <h3>{saleDate}</h3>
-      </div>
-      <div data-testid={ `seller_orders__element-card-price-${id}` }>
-        <h2>{`${totalPrice}`.replace('.', ',')}</h2>
-      </div>
-      <div data-testid={ `seller_orders__element-card-address-${id}` }>
-        <p>{ `${deliveryAddress}, ${deliveryNumber}`}</p>
+      <div
+        className="saleInformation"
+        data-testid={ `seller_orders__element-order-date-${id}` }
+      >
+        <strong>{saleDate}</strong>
+        <strong
+          data-testid={ `seller_orders__element-card-price-${id}` }
+        >
+          {`${totalPrice}`.replace('.', ',')}
+        </strong>
+        <p
+          data-testid={ `seller_orders__element-card-address-${id}` }
+        >
+          { `${deliveryAddress}, ${deliveryNumber}`}
+        </p>
       </div>
     </div>
   );
