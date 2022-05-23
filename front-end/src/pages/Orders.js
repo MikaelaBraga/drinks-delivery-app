632aa8bcf080e1bda6e981b9bb17ce7d9a1972c9
MIkaela Braga <bragamikaela@gmail.com>
2022-05-22 23:36:40 -0300

    feat: initialize order page implementation

---
+++ front-end/src/pages/Orders.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/navBar/navBar';
+import Orders from '../components/orders/Orders';
+
+function OrdersPage() {
+  return (
+    <>
+      <Navbar />
+      <Orders />
+    </>
+  );
+}
+
+export default OrdersPage;
