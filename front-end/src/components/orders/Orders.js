1c8c4cc2c4e94dbe92bdc7b6adc797b383c867d8
MIkaela Braga <bragamikaela@gmail.com>
2022-05-23 00:19:32 -0300

    feat: add link card redirect to order details

--- front-end/src/components/orders/Orders.js
+++ front-end/src/components/orders/Orders.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useRequestOrders from '../hooks/orders/useRequestOrders';
 
 function Orders() {
@@ -7,28 +8,30 @@ function Orders() {
   return (
     <div>
       { orders && orders.map((order, index) => (
-        <div key={ index }>
-          <p
-            data-testid={ `customer_orders__element-order-id-${order.id}` }
-          >
-            { `Pedido 00${index + 1}` }
-          </p>
-          <h3
-            data-testid={ `customer_orders__element-delivery-status-${order.id}` }
-          >
-            { order.status }
-          </h3>
-          <h5
-            data-testid={ `customer_orders__element-order-date-${order.id}` }
-          >
-            { order.saleDate }
-          </h5>
-          <h5
-            data-testid={ `customer_orders__element-card-price-${order.id}` }
-          >
-            { order.totalPrice }
-          </h5>
-        </div>
+        <Link to={ `/customer/orders/${order.id}` } key={ index }>
+          <div key={ order.id }>
+            <p
+              data-testid={ `customer_orders__element-order-id-${order.id}` }
+            >
+              { `Pedido 00${index + 1}` }
+            </p>
+            <h3
+              data-testid={ `customer_orders__element-delivery-status-${order.id}` }
+            >
+              { order.status }
+            </h3>
+            <h5
+              data-testid={ `customer_orders__element-order-date-${order.id}` }
+            >
+              { order.saleDate }
+            </h5>
+            <h5
+              data-testid={ `customer_orders__element-card-price-${order.id}` }
+            >
+              { order.totalPrice }
+            </h5>
+          </div>
+        </Link>
       )) }
     </div>
   );
