9dae915ddcda9662fc8b699204a395ef568b1f2f
MIkaela Braga <bragamikaela@gmail.com>
2022-05-23 00:15:26 -0300

    feat: add data-testid

--- front-end/src/components/orders/Orders.js
+++ front-end/src/components/orders/Orders.js
@@ -3,14 +3,31 @@ import useRequestOrders from '../hooks/orders/useRequestOrders';
 
 function Orders() {
   const [orders] = useRequestOrders();
+  console.log(orders);
   return (
     <div>
       { orders && orders.map((order, index) => (
         <div key={ index }>
-          <p>{ `Pedido 00${index + 1}` }</p>
-          <h3>{ order.status }</h3>
-          <h5>{ order.saleDate }</h5>
-          <h5>{ order.totalPrice }</h5>
+          <p
+            data-testid={ `customer_orders__element-order-id-${order.id}` }
+          >
+            { `Pedido 00${index + 1}` }
+          </p>
+          <h3
+            data-testid={ `customer_orders__element-delivery-status-${order.id}` }
+          >
+            { order.status }
+          </h3>
+          <h5
+            data-testid={ `customer_orders__element-order-date-${order.id}` }
+          >
+            { order.saleDate }
+          </h5>
+          <h5
+            data-testid={ `customer_orders__element-card-price-${order.id}` }
+          >
+            { order.totalPrice }
+          </h5>
         </div>
       )) }
     </div>
