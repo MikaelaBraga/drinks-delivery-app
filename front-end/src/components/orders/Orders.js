9ed863a587e95e7fd9672ea2fd19537a1e00a199
MIkaela Braga <bragamikaela@gmail.com>
2022-05-22 23:48:41 -0300

    feat: implements order cards values

--- front-end/src/components/orders/Orders.js
+++ front-end/src/components/orders/Orders.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import useRequestOrders from '../hooks/orders/useRequestOrders';
 
 function Orders() {
-  return ();
+  const [orders] = useRequestOrders();
+  return (
+    <div>
+      { orders && orders.map((order, index) => (
+        <div key={ index }>
+          <p>{ `Pedido 00${index + 1}` }</p>
+          <h3>{ order.status }</h3>
+          <h5>{ order.saleDate }</h5>
+          <h5>{ order.totalPrice }</h5>
+        </div>
+      )) }
+    </div>
+  );
 }
 
 export default Orders;
