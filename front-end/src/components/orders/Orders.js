952f5e6772624d6d1be5614a4a6e30085e966271
MIkaela Braga <bragamikaela@gmail.com>
2022-05-25 19:39:59 -0300

    refactor: change order data consumptiom to propTypes

--- front-end/src/components/orders/Orders.js
+++ front-end/src/components/orders/Orders.js
@@ -1,40 +1,57 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import useRequestOrders from '../hooks/orders/useRequestOrders';
 
-function Orders() {
-  const [orders] = useRequestOrders();
+function Orders(props) {
+  const { order, index } = props;
+  const { id, status, saleDate, totalPrice } = order;
+
+  const dateInput = saleDate;
+  const newDate = new Date(dateInput);
 
   return (
     <div>
-      { orders && orders.map((order, index) => (
-        <Link to={ `/customer/orders/${order.id}` } key={ index }>
-          <div key={ order.id }>
-            <p
-              data-testid={ `customer_orders__element-order-id-${order.id}` }
-            >
-              { `Pedido 00${index + 1}` }
-            </p>
-            <h3
-              data-testid={ `customer_orders__element-delivery-status-${order.id}` }
-            >
-              { order.status }
-            </h3>
-            <h5
-              data-testid={ `customer_orders__element-order-date-${order.id}` }
-            >
-              { order.saleDate }
-            </h5>
-            <h5
-              data-testid={ `customer_orders__element-card-price-${order.id}` }
-            >
-              { order.totalPrice }
-            </h5>
-          </div>
-        </Link>
-      )) }
+      <Link
+        to={ `/customer/orders/${id}` }
+        key={ index }
+        style={ { textDecoration: 'none', color: 'black' } }
+      >
+        <div key={ id }>
+          <p
+            data-testid={ `customer_orders__element-order-id-${id}` }
+          >
+            { `Pedido 00${index + 1}` }
+          </p>
+          <h3
+            data-testid={ `customer_orders__element-delivery-status-${id}` }
+          >
+            { status }
+          </h3>
+          <h5
+            data-testid={ `customer_orders__element-order-date-${id}` }
+          >
+            { newDate.toLocaleDateString('pt-BR', { timeZone: 'America/Sao_Paulo' }) }
+          </h5>
+          <h5
+            data-testid={ `customer_orders__element-card-price-${id}` }
+          >
+            { `${totalPrice}`.replace('.', ',') }
+          </h5>
+        </div>
+      </Link>
+
     </div>
   );
 }
+
+Orders.propTypes = {
+  order: PropTypes.shape({
+    id: PropTypes.number,
+    status: PropTypes.string,
+    saleDate: PropTypes.string,
+    totalPrice: PropTypes.number,
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+};
 
 export default Orders;
--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from '../components/navBar/navBar';
 import Orders from '../components/orders/Orders';
+import useRequestOrders from '../components/hooks/orders/useRequestOrders';
 
 function OrdersPage() {
+  const [orders] = useRequestOrders();
+
   return (
     <>
       <Navbar />
-      <Orders />
+      { orders && orders.map((order, index) => (
+        <Orders order={ order } index={ index } key={ index } />
+      )) }
     </>
   );
 }
