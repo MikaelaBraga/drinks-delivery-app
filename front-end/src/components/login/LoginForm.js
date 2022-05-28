a5a6e7bdb6ff849b0a07b72ad3e19755b527727b
MIkaela Braga <bragamikaela@gmail.com>
2022-05-27 21:29:05 -0300

    styles: add login page style

--- front-end/src/components/login/LoginForm.js
+++ front-end/src/components/login/LoginForm.js
@@ -4,6 +4,8 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigate, Link } from 'react-router-dom';
 import loginValidate from './validate/loginValidate';
 import api from '../../services/api';
+import deliveryDrinksLogo from '../../images/drinksDelivery.png';
+import './login.css';
 
 function Login() {
   const { register, handleSubmit, formState: { errors, isDirty, isValid } } = useForm({
@@ -43,53 +45,65 @@ function Login() {
     .catch(({ response }) => setInvalidLogin(response.data));
 
   return (
-    <form onSubmit={ handleSubmit(onSubmit) }>
-      <div>
-        <label htmlFor="email">
-          Email
-          <input
-            data-testid="common_login__input-email"
-            placeholder="Insira seu email"
-            name="email"
-            type="email"
-            { ...register('email') }
-          />
-        </label>
-        <p>{ errors.email?.message }</p>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Senha
-          <input
-            data-testid="common_login__input-password"
-            placeholder="Insira sua senha"
-            name="password"
-            type="password"
-            { ...register('password') }
-          />
-        </label>
-        <p>{ errors.password?.message }</p>
-      </div>
-      <div>
-        <p data-testid="common_login__element-invalid-email">{ invalidLogin?.message }</p>
-      </div>
-      <button
-        data-testid="common_login__button-login"
-        type="submit"
-        disabled={ !isDirty || !isValid }
-      >
-        Login
-      </button>
+    <div className="container-login">
 
-      <button
-        onClick={ () => navigate('/register') }
-        data-testid="common_login__button-register"
-        type="button"
-      >
-        <Link to="/register">Ainda não tenho conta</Link>
-      </button>
+      <img src={ deliveryDrinksLogo } alt="logo delivery drinks" />
+      <h1 className="title-login">Login</h1>
 
-    </form>
+      <form className="form-login" onSubmit={ handleSubmit(onSubmit) }>
+        <div>
+          <label htmlFor="email">
+            Email
+            <input
+              data-testid="common_login__input-email"
+              placeholder="Insira seu email"
+              name="email"
+              type="email"
+              { ...register('email') }
+            />
+          </label>
+          <p>{ errors.email?.message }</p>
+        </div>
+        <div>
+          <label htmlFor="password">
+            Senha
+            <input
+              data-testid="common_login__input-password"
+              placeholder="Insira sua senha"
+              name="password"
+              type="password"
+              { ...register('password') }
+            />
+          </label>
+          <p>{ errors.password?.message }</p>
+        </div>
+        <div>
+          <p
+            data-testid="common_login__element-invalid-email"
+          >
+            { invalidLogin?.message }
+          </p>
+        </div>
+        <button
+          className="button-login"
+          data-testid="common_login__button-login"
+          type="submit"
+          disabled={ !isDirty || !isValid }
+        >
+          Login
+        </button>
+
+        <button
+          className="button-register"
+          onClick={ () => navigate('/register') }
+          data-testid="common_login__button-register"
+          type="button"
+        >
+          <Link to="/register">Ainda não tenho conta</Link>
+        </button>
+
+      </form>
+    </div>
   );
 }
 
