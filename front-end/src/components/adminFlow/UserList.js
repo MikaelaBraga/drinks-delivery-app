8d1003e67198d0807f61d87478bb605a2b88c7a9
MIkaela Braga <bragamikaela@gmail.com>
2022-05-30 22:58:40 -0300

    style: add admin page user list style

--- front-end/src/components/adminFlow/UserList.js
+++ front-end/src/components/adminFlow/UserList.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { AdminContext } from '../../context/AdminProvider';
 import api from '../../services/api';
+import './adminUserList.css';
 
 function UserList() {
   const { token } = JSON.parse(localStorage.getItem('user'));
@@ -18,7 +19,7 @@ function UserList() {
   useEffect(() => {}, [users]);
 
   return (
-    <>
+    <div className="container-user-table">
       <h1>Lista de usuários</h1>
 
       <table>
@@ -35,26 +36,30 @@ function UserList() {
           { users.length > 0 && users.map((user, index) => (
             <tr key={ index }>
               <td
+                className="admin-table-item"
                 data-testid={ `admin_manage__element-user-table-item-number-${index}` }
               >
                 { index + 1 }
               </td>
               <td
+                className="admin-table-name"
                 data-testid={ `admin_manage__element-user-table-name-${index}` }
               >
                 { user.name }
               </td>
               <td
+                className="admin-table-email"
                 data-testid={ `admin_manage__element-user-table-email-${index}` }
               >
                 { user.email }
               </td>
               <td
+                className="admin-table-typeUser"
                 data-testid={ `admin_manage__element-user-table-role-${index}` }
               >
                 { user.role }
               </td>
-              <td>
+              <td className="admin-table-removeUser">
                 <button
                   type="button"
                   data-testid={ `admin_manage__element-user-table-remove-${index}` }
@@ -67,7 +72,7 @@ function UserList() {
           ))}
         </tbody>
       </table>
-    </>
+    </div>
 
   );
 }
